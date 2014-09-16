ad940b42a766f75689e1a065a542f115d828d96a
Emlyn Clay <eclay101@gmail.com>
2014-09-16 11:47:33 +0100

    Add the patient_id endpoint

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask.ext import restful
-import os, json
+import json
 
 app = Flask(__name__)
 api = restful.Api(app)
+
+database = json.load(open('data.json'))
 
 class PatientRecords(restful.Resource):
     def get(self):
@@ -11,11 +13,27 @@ class PatientRecords(restful.Resource):
         Get the patient record
         :return:
         """
-        data = json.load(open('data.json'))     # load the data
-        return data
+        return database
+
+class PatientID(restful.Resource):
+    def get(self, patient_id):
+        """
+        Given a patient ID, check the records and append records that match the
+        patient id
+        :param patient_id:
+        :return:
+        """
+        response = []
+        for record in database:
+            record = dict(record)
+            if record['patient_id'] == patient_id:
+                response.append(record)
+
+        return json.dumps(response)
 
 # API endpoints
 api.add_resource(PatientRecords, '/')
+api.add_resource(PatientID, '/patient/<int:patient_id>/')
 
 
 if __name__ == '__main__':
