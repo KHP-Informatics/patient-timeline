ac5ab0416369caba72c5051eeb695e827f7324e1
Emlyn Clay <eclay101@gmail.com>
2014-09-16 15:10:27 +0100

    Filter by start and end date

--- server.py
+++ server.py
@@ -5,7 +5,9 @@ import json, datetime
 app = Flask(__name__)
 api = restful.Api(app)
 
+# config
 database = json.load(open('data.json'))
+date_fmt = '%Y-%m-%d'
 
 def reindex(data=database, key='patient_id', primary_key=True):
     """
@@ -28,22 +30,7 @@ def reindex(data=database, key='patient_id', primary_key=True):
         else:
             indexed.append({'date': record['date'], "element": [record]})
 
-    # sort the records by date
-    # for subject in indexed:
-    #     sorted_entries = []
-    #
-    #     for record in subject:
-    #         record = sorted(record, key=lambda k: k['date'])
-    #         sorted_entries.append(record)
-    #
-    #     sorted_indexed[subject] = sorted_indexed
-    #
-    # indexed = sorted_indexed
-
     return indexed
-
-def sort_by_value():
-    pass
 
 class PatientRecords(restful.Resource):
     def get(self):
@@ -54,24 +41,52 @@ class PatientRecords(restful.Resource):
         return reindex(database)
 
 class PatientID(restful.Resource):
-    def get(self, patient_id):
+    def get(self, patient_id, start_date=None, end_date=None):
         """
         Given a patient ID, check the records and append records that match the
         patient id
         :param patient_id:
         :return:
         """
+        # start_date = False
+        # end_date = False
+
         response = []
         for record in database:
             record = dict(record)
             if int(record['patient_id']) == patient_id:
-                response.append(record)
+
+                if start_date:
+                    # convert start_date and the date in the record to a
+                    # python datetime
+                    start = datetime.datetime.strptime(start_date, date_fmt)
+                    record_date = datetime.datetime.strptime(record['date'], date_fmt)
+
+                    if record_date > start:
+                        response.append(record)
+
+                elif start_date and end_date:
+                    start = datetime.datetime.strptime(start_date, date_fmt)
+                    end = datetime.datetime.strptime(end_date, date_fmt)
+                    record_date = datetime.datetime.strptime(record['date'], date_fmt)
+
+                    if start > record_date < end:
+                        response.append(record)
+
+                else:
+                    response.append(record)
 
         return sorted(reindex(response, 'date', False), key=lambda x: x['date'])
 
+
+
 # API endpoints
 api.add_resource(PatientRecords, '/')
-api.add_resource(PatientID, '/patient/<int:patient_id>/')
+api.add_resource(PatientID,
+                 '/patient/<int:patient_id>/',
+                 '/patient/<int:patient_id>/<string:start_date>/',
+                 '/patient/<int:patient_id>/<string:start_date>/<string:end_date>/',
+                 )
 
 
 if __name__ == '__main__':
